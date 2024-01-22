ba35e6b7b368185c7a215ec546559b1482d389ee
jgrynczewski <jgrynczewski@gmail.com>
2024-01-22 14:37:12 +0100

    zalecznosc vs kompozycja

--- farm.py
+++ farm.py
@@ -7,11 +7,12 @@ def func_name():
 # Camel case (klas)
 # ToJesteNowaNazwaMojejKlasy
 class Cow:
-    def __init__(self, name, age=0):
+    def __init__(self, name, account, age=0):
         self.is_alive = True
         self.name = name
         self.age = age
-        self.places = []
+        # Kompozycja (ang. composition) (obiekt klasy Cow komponuje się/składa się/ma (HAS-A) obiekt klasy Account)
+        self.account = account
 
     def speak(self):
         if self.is_alive:
@@ -28,15 +29,41 @@ class Cow:
     def get_age(self):
         return self.age
 
-    def append_place(self, place_name):
-        self.places.append(place_name)
+    # Zależność (ang. dependency) (obiekt klasy Cow używa (USE-A) obiektu klasy Place)
+    def visit_place(self, place):
+        print(f"O jakie piękne miejsce {place.name} ({place.type})")
+
+    def sum_age(self, other_cow):
+        result = self.age + other_cow.age
+        print(f"Razem mamy {result}")
+
+
+class Place:
+    def __init__(self, type, name):
+        self.type = type
+        self.name = name
+
+
+class Account:
+    def __init__(self, amount):
+        self.amount = amount
+
 
 # ============================
 # ============================
 
+place1 = Place('pastwisko', 'numer 3')
+place2 = Place('pastwisko', 'numer 5')
+place3 = Place('pastwisko', 'numer 9')
 
-first_cow = Cow("mućka", 2)
-second_cow = Cow("milka")
+place4 = Place('stodola', 'kwiecista')
+place5 = Place('stodola', 'złota')
+
+account1 = Account(100)
+account2 = Account(200)
+
+first_cow = Cow("mućka", account1, 2)
+second_cow = Cow("milka", account2)
 
 first_cow.speak()
 second_cow.speak()
@@ -48,9 +75,12 @@ first_cow.speak()
 second_cow.speak()
 
 result = second_cow.get_age()
-print(second_cow.places)
-second_cow.append_place("Pastwisko nr 3")
-second_cow.append_place("Stodola 2")
-print(second_cow.places)
-
 print(result)
+
+# powiązania
+# Dependency (zależność, relacja typu USE-A - używa)
+# Obiekt klasy Cow używa (USE-A) obiektu klasy Place
+second_cow.visit_place(place1)
+second_cow.visit_place(place5)
+
+second_cow.sum_age(first_cow)
