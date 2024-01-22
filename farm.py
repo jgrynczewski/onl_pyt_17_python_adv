d2fd777a5e32306f7a9f61d8bd0c352b24326391
jgrynczewski <jgrynczewski@gmail.com>
2024-01-22 12:14:19 +0100

    4 parametry w metodach

--- farm.py
+++ farm.py
@@ -8,18 +8,32 @@ def func_name():
 # ToJesteNowaNazwaMojejKlasy
 class Cow:
     def __init__(self, name, age=0):
-        self.type = 'mammals'
+        self.is_alive = True
         self.name = name
         self.age = age
+        self.places = []
 
     def speak(self):
-        print(f"Muuuuuuuuuuu. Cześć jestem {self.name}")
+        if self.is_alive:
+            print(f"Muuuuuuuuuuu. Cześć jestem {self.name}")
+        else:
+            print("RIP")
 
     def get_older(self):
-        self.age += 1
+        if self.is_alive:
+            self.age += 1
+            if self.age > 10:
+                self.is_alive = False
+
+    def get_age(self):
+        return self.age
+
+    def append_place(self, place_name):
+        self.places.append(place_name)
 
 # ============================
 # ============================
+
 
 first_cow = Cow("mućka", 2)
 second_cow = Cow("milka")
@@ -27,6 +41,16 @@ second_cow = Cow("milka")
 first_cow.speak()
 second_cow.speak()
 
-print(first_cow.age)
-first_cow.get_older()
-print(first_cow.age)
+for _ in range(100):
+    first_cow.get_older()
+
+first_cow.speak()
+second_cow.speak()
+
+result = second_cow.get_age()
+print(second_cow.places)
+second_cow.append_place("Pastwisko nr 3")
+second_cow.append_place("Stodola 2")
+print(second_cow.places)
+
+print(result)
