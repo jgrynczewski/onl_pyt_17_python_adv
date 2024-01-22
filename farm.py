cb38651424c92337a7506a997bfed2173fa37091
jgrynczewski <jgrynczewski@gmail.com>
2024-01-22 10:26:36 +0100

    1 Tworzenie zwierzaka

---
+++ farm.py
@@ -0,0 +1,18 @@
+# snake case (zmienne, funkcje, moduły i pakiety)
+# to_jest_nowa_nazwa_mojej_zmiennej
+def func_name():
+    pass
+
+
+# Camel case (klas)
+# ToJesteNowaNazwaMojejKlasy
+class Cow:
+    def speak(self):
+        print("Muuuuuuuuuuu")
+
+
+c1 = Cow()
+c2 = Cow()
+
+c1.speak()
+c2.speak()
