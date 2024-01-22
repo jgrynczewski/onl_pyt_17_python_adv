a4bc19571cfddab6a561b0b32a05710df09c237b
jgrynczewski <jgrynczewski@gmail.com>
2024-01-22 17:05:35 +0100

    data hiding part 2

---
+++ main.py
@@ -0,0 +1,15 @@
+from data_hiding import Account
+
+a = Account(12345677356)
+print(a.get_cash)
+a.set_cash(100)
+print(a.get_cash())
+
+a.set_cash(2.34)
+print(a.get_cash())
+
+a.set_cash(-23523465928)
+print(a.get_cash())
+
+a.set_cash("ala ma kota")
+print(a.get_cash())
